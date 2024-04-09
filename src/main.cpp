a0e74f453aba090f1ed6247b13f11a40cf4069b6
Szymon Rzazewski <srzazewski@sii.pl>
2024-04-09 15:33:51 +0200

    02.01 Program structure

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,15 @@
 
 int main() 
 { 
-    std::cout << "Hello world." << std::endl;
+    int i = 1;
+	char c = 'a';
+
+    std::cout << "Hello in C++ program!\n";
+	std::cout << "i = " << i << "\n";
+	std::cout << "c = " << c << "\n";
+    std::cout << "i = " << i << "\nc = " << c << "\n";
+    std::cout << "i = " << i << std::endl;
+	std::cout << "c = " << c << std::endl;
+    
     return 0; 
 }
