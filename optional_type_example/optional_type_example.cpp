f9f70ac0ab13388bad38b413c5ab7887dabadadc
SRzazewski <sz.rzazewski@gmail.com>
2024-04-23 10:11:52 +0200

    xa.02 - optional type example

---
+++ optional_type_example/optional_type_example.cpp
@@ -0,0 +1,35 @@
+#include <optional>
+#include <limits>
+#include <iostream>
+
+std::optional<int> safe_divide(int x, int y)
+{
+	if (y == 0)
+		return std::nullopt; // return {}; works too
+
+	if (x == std::numeric_limits<int>::min() && y == -1)
+		return std::nullopt; // return {}; works too
+
+	return x / y;
+}
+
+int main()
+{
+    int x1 = 0;
+    int x2 = 0;
+    int operation = 0;
+	std::cout << "Put 1st number:";
+    std::cin >> x1;
+	std::cout << "Put 2nd number:";
+    std::cin >> x2;
+
+	std::optional<int> result = safe_divide(x1, x2);
+	if (result)
+	{
+		std::cout << x1 << " / " << x2 << " = " << *result;
+	}
+	else
+	{
+		std::cout << "Error\n";
+	}
+}
