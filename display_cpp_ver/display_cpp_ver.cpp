6c92dca91d03e3484674e34e1b4580741bc589b6
SRzazewski <sz.rzazewski@gmail.com>
2024-05-12 21:41:49 +0200

    Refactor of display_cpp_ver.cpp

--- display_cpp_ver/display_cpp_ver.cpp
+++ display_cpp_ver/display_cpp_ver.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <stdio.h>
 
 int main()
-{	
-	char cpp_ver[7];
-	sprintf(cpp_ver, "%ld", __cplusplus);
-	std::cout << "Hello from C++" << cpp_ver[2] << cpp_ver[3];
-	std::cout << "\nHello from C++" << __cplusplus / 100 % 100;
+{    
+    char cpp_ver[7];
+    snprintf(cpp_ver, sizeof(cpp_ver)/sizeof(char), "%ld", __cplusplus);
+    std::cout << "Hello from C++" << cpp_ver[2] << cpp_ver[3];
+    std::cout << "\nHello from C++" << __cplusplus / 100 % 100;
 }
