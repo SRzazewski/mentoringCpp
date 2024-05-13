1a8af2f25dafc885c6394327208a700ea6c778a6
SRzazewski <sz.rzazewski@gmail.com>
2024-05-13 23:04:45 +0200

    SFML test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,48 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() 
-{ 
-    std::cout << "Hello world." << std::endl;
-    return 0; 
+int main()
+{
+    auto window = sf::RenderWindow{ { 800u, 600u }, "SFML TEST" };
+    window.setFramerateLimit(144);
+
+    sf::Text text;
+
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf"))
+    {
+        window.close();
+    }
+
+    // select the font
+    text.setFont(font); // font is a sf::Font
+
+    // set the string to display
+    text.setString("Hello world in mentoring app");
+
+    // set the character size
+    text.setCharacterSize(24); // in pixels, not points!
+
+    // set the color
+    text.setFillColor(sf::Color::Red);
+
+    // set the text style
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        
+
+        window.clear();
+        // inside the main loop, between window.clear() and window.display()
+        window.draw(text);
+        window.display();
+    }
 }
